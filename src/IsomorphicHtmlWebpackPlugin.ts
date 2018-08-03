3d139010c934daf9135f6277da9468629c070478
Maciej Chałapuk <maciej@chalapuk.pl>
2018-08-04 01:04:31 +0200

    + build env

---
+++ src/IsomorphicHtmlWebpackPlugin.ts
@@ -0,0 +1,10 @@
+
+import { Plugin, Compiler } from 'webpack';
+
+export class IsomorphicHtmlWebpackPlugin implements Plugin {
+  apply(compiler : Compiler) {
+  }
+}
+
+export default IsomorphicHtmlWebpackPlugin;
+
