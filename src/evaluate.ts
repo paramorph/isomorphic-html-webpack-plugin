c7948601c3efe798544f10e1dc4b28f3b93be63a
Maciej Chałapuk <maciej@chalapuk.pl>
2019-04-23 00:18:32 +0200

    + prepared global namespace in evaluatino context

--- src/evaluate.ts
+++ src/evaluate.ts
@@ -10,6 +10,8 @@ const Module = require('module');
  * @return exports
  */
 export function evaluate(source : string, moduleName : string, globals : any) : any {
+  const { version, arch, platform, release, env } = process;
+
   const sandbox = {
     ...globals,
   };
@@ -29,9 +31,17 @@ export function evaluate(source : string, moduleName : string, globals : any) : 
       sandbox.exports = val;
     },
   };
+  sandbox.process = {
+    title: moduleName,
+    version,
+    arch,
+    platform,
+    release,
+    env,
+  };
   sandbox.global = sandbox;
 
-  const script = new Script(removeShebang(source));
+  const script = new Script(`${makeGlobals};makeGlobals(global);${removeShebang(source)}`);
   script.runInNewContext(sandbox, { filename: moduleName, displayErrors: true });
 
   return sandbox.exports;
@@ -60,5 +70,75 @@ function requireLike(path : string) {
 
 function removeShebang(source : string) {
 	return source.replace(/^\#\!.*/, '');
+}
+
+function makeGlobals(global : NodeJS.Global) {
+  const { Buffer } = require('buffer');
+  const {
+    setTimeout,
+    clearTimeout,
+    setInterval,
+    clearInterval,
+    setImmediate,
+    clearImmediate,
+  } = require('timers');
+
+  global.Array = Array;
+  global.ArrayBuffer = ArrayBuffer;
+  global.Boolean = Boolean;
+  global.Buffer = Buffer;
+  global.DataView = DataView;
+  global.Date = Date;
+  global.Error = Error;
+  global.EvalError = EvalError;
+  global.Float32Array = Float32Array;
+  global.Float64Array = Float64Array;
+  global.Function = Function;
+  global.GLOBAL = global;
+  global.Int16Array = Int16Array;
+  global.Int32Array = Int32Array;
+  global.Int8Array = Int8Array;
+  global.Intl = Intl;
+  global.JSON = JSON;
+  global.Map = Map;
+  global.Math = Math;
+  global.Number = Number;
+  global.Object = Object;
+  global.Promise = Promise;
+  global.RangeError = RangeError;
+  global.ReferenceError = ReferenceError;
+  global.RegExp = RegExp;
+  global.Set = Set;
+  global.String = String;
+  global.Symbol = Symbol;
+  global.SyntaxError = SyntaxError;
+  global.TypeError = TypeError;
+  global.URIError = URIError;
+  global.Uint16Array = Uint16Array;
+  global.Uint32Array = Uint32Array;
+  global.Uint8Array = Uint8Array;
+  global.Uint8ClampedArray = Uint8ClampedArray;
+  global.WeakMap = WeakMap;
+  global.WeakSet = WeakSet;
+  global.clearImmediate = clearImmediate;
+  global.clearInterval = clearInterval;
+  global.clearTimeout = clearTimeout;
+  global.console = console;
+  global.decodeURI = decodeURI;
+  global.decodeURIComponent = decodeURIComponent;
+  global.encodeURI = encodeURI;
+  global.encodeURIComponent = encodeURIComponent;
+  global.escape = escape;
+  global.eval = eval;
+  global.global = global;
+  global.isFinite = isFinite;
+  global.isNaN = isNaN;
+  global.parseFloat = parseFloat;
+  global.parseInt = parseInt;
+  global.root = global;
+  global.setImmediate = setImmediate;
+  global.setInterval = setInterval;
+  global.setTimeout = setTimeout;
+  global.unescape = unescape;
 }
 
